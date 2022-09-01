6393dcded5253bfc484e7c539dfa11c5f6f8140e
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-02 00:59:00 +0600

    Handling Responses using State

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component{
+  state = {
+    dishes : null,
+    errMess : null
+  }
+
   componentDidMount(){
-    // axios.get('http://localhost:3001/dishes')
-    // .then(response => response.data)
-    // .then(data => console.log(data))
-    // .catch(error => console(error.message));
+    console.log("ComponentDidMount : ", this.state);
+    axios.get('http://localhost:3001/dishes')
+    .then(response => response.data)
+    .then(data => {
+      this.setState({
+        dishes:data
+      });
+    })
+    .catch(error => {
+      this.setState({
+        errMess: error.message
+      });
+    });
 
     // axios.post(' http://localhost:3001/dishes',{name:"Pizza", price:"799"})
     // .then(response => console.log(response));
@@ -15,11 +29,12 @@ class App extends Component{
     //  axios.put(' http://localhost:3001/dishes/1',{name:"Cheese cake", price:"250"})
     //  .then(response => console.log(response));
 
-    axios.delete(' http://localhost:3001/dishes/0')
-    .then(response => console.log(response));
-
-
+    // axios.delete(' http://localhost:3001/dishes/0')
+    // .then(response => console.log(response));
   }
+componentDidUpdate(){
+  console.log("Update : ", this.state);
+}
   render(){
     return (
       <div className="App">
